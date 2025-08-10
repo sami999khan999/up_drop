d9d2687ad584698b47de006358319e1d672db12c
sami999khan999 <sami999khan999@gmail.com>
2025-08-10 22:17:20 +0600

    build error fixed

--- frontend/src/app/(root)/(auth)/sso-callback/page.tsx
+++ frontend/src/app/(root)/(auth)/sso-callback/page.tsx
@@ -1,21 +1,23 @@
 "use client";
 
 import Spiner from "@/components/ui/Spiner";
-import { AuthenticateWithRedirectCallback, useAuth } from "@clerk/nextjs";
+import { AuthenticateWithRedirectCallback } from "@clerk/nextjs";
 
 export default function SsoCallbackPage() {
-  const { isLoaded } = useAuth();
+  // const { isLoaded } = useAuth();
 
-  if (!isLoaded) {
-    return <div>Loading...</div>;
-  }
+  // if (!isLoaded) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
-    <div className="h-screen">
-      <AuthenticateWithRedirectCallback />
-      <div className="flex items-center justify-center text-text h-screen">
-        <Spiner className="text-32" />
+    <>
+      <div className="h-screen">
+        <AuthenticateWithRedirectCallback />
+        <div className="flex items-center justify-center text-text h-screen">
+          <Spiner className="text-32" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
